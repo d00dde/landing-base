8a21e0da6d626086019adeb292cddedc2110c001
Lopatko Saha <lopatkoaleksandr2@gmail.com>
2023-04-07 02:11:06 +0300

    fix: card styles

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,11 @@ document.querySelector(".credit_number").onclick = async (e) => {
   await navigator.clipboard.writeText(e.target.innerText);
 };
 new Slider(data, (item) => `
-  <div class="slider__card popIt" data-id="${item.name}">
+  <div class="pet-card popIt" data-id="${item.name}">
     <div class="popIt">
       <img class="popIt" src="${item.img}">
     </div>
     <div class="title popIt">${item.name}</div>
-    <div class="secondary-btn popIt">Learn more</div>
+    <button class="secondary-btn popIt">Learn more</button>
   </div>
 `);
