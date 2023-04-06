1c6f1468312e31af41b0b5f10b5985878d1a6bfe
Lopatko Saha <lopatkoaleksandr2@gmail.com>
2023-04-06 21:29:38 +0300

    pull master

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,18 @@
 import "../style/index.scss";
 import { Slider } from "./Slider";
 
-new Slider([1, 2, 3, 4, 5, 6, 7, 8], (item) => `<div class="slider__card">${item}</div>`);
-
-
 document.querySelector(".credit_number").onclick = async (e) => {
   await navigator.clipboard.writeText(e.target.innerText);
 };
+new Slider(data, (item) => `
+  <div class="slider__card">
+    <div>
+      <img src="${item.img}">
+    </div>
+    <div class="title">${item.name}</div>
+    <div class="secondary-btn">Learn more</div>
+  </div>
+`);
+document.querySelector(".primary-btn").onclick = () => {
+  popup.show("Lol");
+}
